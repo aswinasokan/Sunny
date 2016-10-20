c2a6ad012630d2952680165aedd5a00a432f0576
aswinasokan <aswinasokan@gmail.com>
2016-10-20 16:56:55 +0100

    Commented unused method with Deprecated warning. Removed "public" declaration as class can be package local

--- app/src/main/java/com/latestarter/sunny/data/WeatherContract.java
+++ app/src/main/java/com/latestarter/sunny/data/WeatherContract.java
@@ -21,58 +21,58 @@ import android.text.format.Time;
 /**
  * Defines table and column names for the weather database.
  */
-public class WeatherContract {
+class WeatherContract {
 
     // To make it easy to query for the exact date, we normalize all dates that go into
     // the database to the start of the the Julian day at UTC.
-    private static long normalizeDate(long startDate) {
-        // normalize the start date to the beginning of the (UTC) day
-        Time time = new Time();
-        time.set(startDate);
-        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
-        return time.setJulianDay(julianDay);
-    }
+//    private static long normalizeDate(long startDate) {
+//        // normalize the start date to the beginning of the (UTC) day
+//        Time time = new Time();
+//        time.set(startDate);
+//        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+//        return time.setJulianDay(julianDay);
+//    }
 
     /*
         Inner class that defines the table contents of the location table
         Students: This is where you will add the strings.  (Similar to what has been
         done for WeatherEntry)
      */
-    public static final class LocationEntry implements BaseColumns {
-        public static final String TABLE_NAME = "location";
+    static final class LocationEntry implements BaseColumns {
+        static final String TABLE_NAME = "location";
 
     }
 
     /* Inner class that defines the table contents of the weather table */
-    public static final class WeatherEntry implements BaseColumns {
+    static final class WeatherEntry implements BaseColumns {
 
-        public static final String TABLE_NAME = "weather";
+        static final String TABLE_NAME = "weather";
 
         // Column with the foreign key into the location table.
-        public static final String COLUMN_LOC_KEY = "location_id";
+        static final String COLUMN_LOC_KEY = "location_id";
         // Date, stored as long in milliseconds since the epoch
-        public static final String COLUMN_DATE = "date";
+        static final String COLUMN_DATE = "date";
         // Weather id as returned by API, to identify the icon to be used
-        public static final String COLUMN_WEATHER_ID = "weather_id";
+        static final String COLUMN_WEATHER_ID = "weather_id";
 
         // Short description and long description of the weather, as provided by API.
         // e.g "clear" vs "sky is clear".
-        public static final String COLUMN_SHORT_DESC = "short_desc";
+        static final String COLUMN_SHORT_DESC = "short_desc";
 
         // Min and max temperatures for the day (stored as floats)
-        public static final String COLUMN_MIN_TEMP = "min";
-        public static final String COLUMN_MAX_TEMP = "max";
+        static final String COLUMN_MIN_TEMP = "min";
+        static final String COLUMN_MAX_TEMP = "max";
 
         // Humidity is stored as a float representing percentage
-        public static final String COLUMN_HUMIDITY = "humidity";
+        static final String COLUMN_HUMIDITY = "humidity";
 
         // Humidity is stored as a float representing percentage
-        public static final String COLUMN_PRESSURE = "pressure";
+        static final String COLUMN_PRESSURE = "pressure";
 
         // Windspeed is stored as a float representing windspeed  mph
-        public static final String COLUMN_WIND_SPEED = "wind";
+        static final String COLUMN_WIND_SPEED = "wind";
 
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
-        public static final String COLUMN_DEGREES = "degrees";
+        static final String COLUMN_DEGREES = "degrees";
     }
 }
